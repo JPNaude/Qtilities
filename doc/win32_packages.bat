0638c8b2254f4144c32a1b8eb2b094228738d2a4
Jaco Naude <naude.jaco@gmail.com>
2010-08-18 05:32:22 +0000

    Documentation script files now copies the tab images to the output folders.

--- doc/win32_packages.bat
+++ doc/win32_packages.bat
@@ -1,4 +1,7 @@
 doxygen doxyfile_packages
-cd packages_html
+cd output
+cd html_packages
 C:\Tools\Qt\2010.03\qt\bin\qhelpgenerator index.qhp -o qtilities.qch
 cd ..
+cd ..
+copy /y images\tab_*.png output\html_packages
--- doc/win32_website.bat
+++ doc/win32_website.bat
@@ -1,1 +1,2 @@
 doxygen doxyfile_website 
+copy /y images\tab_*.png output\html_website
