83af118a3f9ec5381fb8e533b442cba1221a35ed
Jaco Naude <naude.jaco@gmail.com>
2010-08-19 05:47:07 +0000

    New ObserverWidgetConfig widget in Clipboard Example.

---
+++ src/Examples/ClipboardExample/ObserverWidgetConfig.cpp
@@ -0,0 +1,158 @@
+/****************************************************************************
+**
+** Copyright (c) 2009-2010, Jaco Naude
+**
+** This file is part of Qtilities which is released under the following
+** licensing options.
+**
+** Option 1: Open Source
+** Under this license Qtilities is free software: you can
+** redistribute it and/or modify it under the terms of the GNU General
+** Public License as published by the Free Software Foundation, either
+** version 3 of the License, or (at your option) any later version.
+**
+** Qtilities is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Qtilities. If not, see http://www.gnu.org/licenses/.
+**
+** Option 2: Commercial
+** Alternatively, this library is also released under a commercial license
+** that allows the development of closed source proprietary applications
+** without restrictions on licensing. For more information on this option,
+** please see the project website's licensing page:
+** http://www.qtilities.org/licensing.html
+**
+** If you are unsure which license is appropriate for your use, please
+** contact support@qtilities.org.
+**
+****************************************************************************/
+
+#include "ObserverWidgetConfig.h"
+#include "ui_ObserverWidgetConfig.h"
+
+Qtilities::Examples::Clipboard::ObserverWidgetConfig::ObserverWidgetConfig(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::ObserverWidgetConfig)
+{
+    ui->setupUi(this);
+
+    // Connections:
+    connect(ui->comboWidgetMode1,SIGNAL(currentIndexChanged(QString)),SLOT(handle_comboWidgetMode1(QString)));
+    connect(ui->comboWidgetMode2,SIGNAL(currentIndexChanged(QString)),SLOT(handle_comboWidgetMode2(QString)));
+
+    // We read the settings of each widget:
+    QSettings settings;
+    settings.beginGroup("GUI");
+    settings.beginGroup("ObserverWidget0");
+    int mode = settings.value("display_mode", ObserverWidget::TableView).toInt();
+    if ((ObserverWidget::DisplayMode) mode == ObserverWidget::TableView)
+        ui->comboWidgetMode1->setCurrentIndex(0);
+    else
+        ui->comboWidgetMode1->setCurrentIndex(1);
+    ui->spinBoxRowHeight1->setValue(settings.value("default_row_heigth", 17).toInt());
+    ui->checkShowGrid1->setChecked(settings.value("table_view_show_grid", false).toBool());
+    settings.endGroup();
+    settings.beginGroup("ObserverWidget1");
+    mode = settings.value("display_mode", ObserverWidget::TableView).toInt();
+    if ((ObserverWidget::DisplayMode) mode == ObserverWidget::TableView)
+        ui->comboWidgetMode2->setCurrentIndex(0);
+    else
+        ui->comboWidgetMode2->setCurrentIndex(1);
+    ui->spinBoxRowHeight2->setValue(settings.value("default_row_heigth", 17).toInt());
+    ui->checkShowGrid2->setChecked(settings.value("table_view_show_grid", false).toBool());
+    settings.endGroup();
+    settings.endGroup();
+
+
+    // Update widget according to settings which was read:
+    if (ui->comboWidgetMode1->currentText() == "Table View") {
+        ui->spinBoxRowHeight1->setEnabled(true);
+    } else {
+        ui->spinBoxRowHeight1->setEnabled(false);
+    }
+    if (ui->comboWidgetMode2->currentText() == "Table View") {
+        ui->spinBoxRowHeight2->setEnabled(true);
+    } else {
+        ui->spinBoxRowHeight2->setEnabled(false);
+    }
+}
+
+Qtilities::Examples::Clipboard::ObserverWidgetConfig::~ObserverWidgetConfig()
+{
+    delete ui;
+}
+
+QIcon Qtilities::Examples::Clipboard::ObserverWidgetConfig::configPageIcon() const {
+    return QIcon();
+}
+
+QWidget* Qtilities::Examples::Clipboard::ObserverWidgetConfig::configPageWidget() {
+    return this;
+}
+
+QStringList Qtilities::Examples::Clipboard::ObserverWidgetConfig::configPageTitle() const {
+    QStringList text;
+    text << "Observer Widgets";
+    return text;
+}
+
+void Qtilities::Examples::Clipboard::ObserverWidgetConfig::configPageApply() {
+    // Write the settings:
+    QSettings settings;
+    settings.beginGroup("GUI");
+    settings.beginGroup("ObserverWidget0");
+    if (ui->comboWidgetMode1->currentText() == "Table View") {
+        settings.setValue("display_mode", (int) ObserverWidget::TableView);
+    } else {
+        settings.setValue("display_mode", (int) ObserverWidget::TreeView);
+    }
+    settings.setValue("default_row_heigth", ui->spinBoxRowHeight1->value());
+    settings.setValue("table_view_show_grid", ui->checkShowGrid1->isChecked());
+    settings.endGroup();
+    settings.beginGroup("ObserverWidget1");
+    if (ui->comboWidgetMode2->currentText() == "Table View") {
+        settings.setValue("display_mode", (int) ObserverWidget::TableView);
+    } else {
+        settings.setValue("display_mode", (int) ObserverWidget::TreeView);
+    }
+    settings.setValue("default_row_heigth", ui->spinBoxRowHeight2->value());
+    settings.setValue("table_view_show_grid", ui->checkShowGrid2->isChecked());
+    settings.endGroup();
+    settings.endGroup();
+
+    // Send the update requests:
+    QtilitiesApplication::newSettingsUpdateRequest("ObserverWidget0");
+    QtilitiesApplication::newSettingsUpdateRequest("ObserverWidget1");
+}
+
+void Qtilities::Examples::Clipboard::ObserverWidgetConfig::handle_comboWidgetMode1(const QString& new_value) {
+    if (new_value == "Table View") {
+        ui->spinBoxRowHeight1->setEnabled(true);
+    } else {
+        ui->spinBoxRowHeight1->setEnabled(false);
+    }
+}
+
+void Qtilities::Examples::Clipboard::ObserverWidgetConfig::handle_comboWidgetMode2(const QString& new_value) {
+    if (new_value == "Table View") {
+        ui->spinBoxRowHeight2->setEnabled(true);
+    } else {
+        ui->spinBoxRowHeight2->setEnabled(false);
+    }
+}
+
+void Qtilities::Examples::Clipboard::ObserverWidgetConfig::changeEvent(QEvent *e)
+{
+    QWidget::changeEvent(e);
+    switch (e->type()) {
+    case QEvent::LanguageChange:
+        ui->retranslateUi(this);
+        break;
+    default:
+        break;
+    }
+}
---
+++ src/Examples/ClipboardExample/ObserverWidgetConfig.h
@@ -0,0 +1,92 @@
+/****************************************************************************
+**
+** Copyright (c) 2009-2010, Jaco Naude
+**
+** This file is part of Qtilities which is released under the following
+** licensing options.
+**
+** Option 1: Open Source
+** Under this license Qtilities is free software: you can
+** redistribute it and/or modify it under the terms of the GNU General
+** Public License as published by the Free Software Foundation, either
+** version 3 of the License, or (at your option) any later version.
+**
+** Qtilities is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Qtilities. If not, see http://www.gnu.org/licenses/.
+**
+** Option 2: Commercial
+** Alternatively, this library is also released under a commercial license
+** that allows the development of closed source proprietary applications
+** without restrictions on licensing. For more information on this option,
+** please see the project website's licensing page:
+** http://www.qtilities.org/licensing.html
+**
+** If you are unsure which license is appropriate for your use, please
+** contact support@qtilities.org.
+**
+****************************************************************************/
+
+#ifndef OBSERVERWIDGETCONFIG_H
+#define OBSERVERWIDGETCONFIG_H
+
+#include <QWidget>
+
+#include <QtilitiesCoreGui>
+using namespace QtilitiesCoreGui;
+
+namespace Ui {
+    class ObserverWidgetConfig;
+}
+
+namespace Qtilities {
+    namespace Examples {
+        //! Namespace containing all the classes which forms part of the Clipboard Example.
+        namespace Clipboard {
+            /*!
+            \class ObserverWidgetConfig
+            \brief The ObserverWidgetConfig widget is used in the Clipboard Example to demonstrate the settings update request capability of QtilitiesApplication.
+
+            QtilitiesApplication::newSettingsUpdateRequest() allows settings update requests to be sent anywhere in an application. This
+            allows objects which depends on the settings to update themselves when the settings updates.
+
+            This class shows options for the two observer widgets found in the Clipboard Example. When the user clicks
+            apply, the widgets will receive the settings update requests and update themselves accordingly. The update requests
+            are delivered by the QtilitiesApplication::instance()->settingsUpdateRequest() signal.
+              */
+            class ObserverWidgetConfig : public QWidget, public IConfigPage {
+                Q_OBJECT
+                Q_INTERFACES(Qtilities::CoreGui::Interfaces::IConfigPage)
+
+            public:
+                ObserverWidgetConfig(QWidget *parent = 0);
+                ~ObserverWidgetConfig();
+
+                // --------------------------------------------
+                // IConfigPage Implementation
+                // --------------------------------------------
+                QIcon configPageIcon() const;
+                QWidget* configPageWidget();
+                QStringList configPageTitle() const;
+                void configPageApply();
+                bool supportsApply() const { return true; }
+
+            public slots:
+                void handle_comboWidgetMode1(const QString& new_value);
+                void handle_comboWidgetMode2(const QString& new_value);
+
+            protected:
+                void changeEvent(QEvent *e);
+
+            private:
+                Ui::ObserverWidgetConfig *ui;
+            };
+        }
+    }
+}
+
+#endif // OBSERVERWIDGETCONFIG_H
