9d188e0dc3acae4242507c80e3af2d23897cb4f7
Jaco Naude <naude.jaco@gmail.com>
2010-08-31 11:55:56 +0000

    Final updates to docs for v0.1 release.

--- doc/win32_packages.bat
+++ doc/win32_packages.bat
@@ -1,9 +1,9 @@
 doxygen doxyfile_packages
+copy /y images\tab_*.png output\html_packages
+copy /y images\bc_s.png output\html_website
 copy /y sources\qtilities.qhcp output\html_packages
 cd output
 cd html_packages
 C:\Tools\Qt\2010.03\qt\bin\qcollectiongenerator qtilities.qhcp -o qtilities.qhc
 cd ..
 cd ..
-copy /y images\tab_*.png output\html_packages
-copy /y images\bc_s.png output\html_website
