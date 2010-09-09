06b73410d232b98450921576a2c8c25b0dad82df
Jaco Naude <naude.jaco@gmail.com>
2010-09-09 20:03:49 +0000

    New ObserverTreeModelProxyFilter class used to filter observer trees.

---
+++ src/CoreGui/source/ObserverTreeModelProxyFilter.cpp
@@ -0,0 +1,84 @@
+/****************************************************************************
+**
+** Copyright (c) 2009-2010, Jaco Naude
+**
+** This file is part of Qtilities which is released under the following
+** licensing options.
+**
+** Option 1: Open Source
+** Under this license Qtilities is free software: you can
+** redistribute it and/or modify it under the terms of the GNU General
+** Public License as published by the Free Software Foundation, either
+** version 3 of the License, or (at your option) any later version.
+**
+** Qtilities is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Qtilities. If not, see http://www.gnu.org/licenses/.
+**
+** Option 2: Commercial
+** Alternatively, this library is also released under a commercial license
+** that allows the development of closed source proprietary applications
+** without restrictions on licensing. For more information on this option,
+** please see the project website's licensing page:
+** http://www.qtilities.org/licensing.html
+**
+** If you are unsure which license is appropriate for your use, please
+** contact support@qtilities.org.
+**
+****************************************************************************/
+
+#include "ObserverTreeModelProxyFilter.h"
+#include "AbstractObserverTreeModel.h"
+#include "QtilitiesCoreGuiConstants.h"
+
+#include <Observer.h>
+#include <QtilitiesCoreConstants.h>
+
+using namespace Qtilities::CoreGui::Constants;
+using namespace Qtilities::Core::Properties;
+using namespace Qtilities::Core;
+using namespace Qtilities::Core::Constants;
+
+Qtilities::CoreGui::ObserverTreeModelProxyFilter::ObserverTreeModelProxyFilter(QObject* parent) : QSortFilterProxyModel(parent) {
+    row_filter_types = ObserverTreeItem::TreeItem;
+}
+
+Qtilities::CoreGui::ObserverTreeModelProxyFilter::~ObserverTreeModelProxyFilter() {
+
+}
+
+bool Qtilities::CoreGui::ObserverTreeModelProxyFilter::filterAcceptsRow(int sourceRow, const QModelIndex &sourceParent) const {
+    AbstractObserverTreeModel* tree_model = dynamic_cast<AbstractObserverTreeModel*> (sourceModel());
+
+    if (tree_model) {
+        // Check if it contains the filterRegExp():
+        QModelIndex name_index = sourceModel()->index(sourceRow, tree_model->columnPosition(AbstractObserverItemModel::ColumnName), sourceParent);
+        ObserverTreeItem* tree_item = tree_model->getItem(name_index);
+        if (tree_item) {
+            if (!(row_filter_types & tree_item->itemType()))
+                return true;
+            else {
+                if (!sourceModel()->data(name_index).toString().contains(filterRegExp()))
+                    return false;
+            }
+        } else {
+            if (!sourceModel()->data(name_index).toString().contains(filterRegExp()))
+                return false;
+        }
+
+        // Category filtering is done during the building of the tree in AbstractObserverTreeModel.
+    }
+    return true;
+}
+
+void Qtilities::CoreGui::ObserverTreeModelProxyFilter::setRowFilterTypes(ObserverTreeItem::TreeItemTypeFlags type_flags) {
+    row_filter_types = type_flags;
+}
+
+Qtilities::CoreGui::ObserverTreeItem::TreeItemTypeFlags Qtilities::CoreGui::ObserverTreeModelProxyFilter::rowFilterTypes() const {
+    return row_filter_types;
+}
---
+++ src/CoreGui/source/ObserverTreeModelProxyFilter.h
@@ -0,0 +1,70 @@
+/****************************************************************************
+**
+** Copyright (c) 2009-2010, Jaco Naude
+**
+** This file is part of Qtilities which is released under the following
+** licensing options.
+**
+** Option 1: Open Source
+** Under this license Qtilities is free software: you can
+** redistribute it and/or modify it under the terms of the GNU General
+** Public License as published by the Free Software Foundation, either
+** version 3 of the License, or (at your option) any later version.
+**
+** Qtilities is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Qtilities. If not, see http://www.gnu.org/licenses/.
+**
+** Option 2: Commercial
+** Alternatively, this library is also released under a commercial license
+** that allows the development of closed source proprietary applications
+** without restrictions on licensing. For more information on this option,
+** please see the project website's licensing page:
+** http://www.qtilities.org/licensing.html
+**
+** If you are unsure which license is appropriate for your use, please
+** contact support@qtilities.org.
+**
+****************************************************************************/
+
+#ifndef OBSERVER_TREE_MODEL_PROXY_FILTER_H
+#define OBSERVER_TREE_MODEL_PROXY_FILTER_H
+
+#include <QSortFilterProxyModel>
+
+#include "Observer.h"
+#include "ObserverTreeItem.h"
+
+namespace Qtilities {
+    namespace CoreGui {
+        /*!
+          \class ObserverTreeModelProxyFilter
+          \brief The ObserverTreeModelProxyFilter class is an implementation of a QSortFilterProxyModel which is used for advanced filtering in AbstractObserverTreeModel.
+          */
+        class ObserverTreeModelProxyFilter : public QSortFilterProxyModel
+        {
+            Q_OBJECT
+
+        public:
+            ObserverTreeModelProxyFilter(QObject* parent = 0);
+            ~ObserverTreeModelProxyFilter();
+
+            //! Sets the tree item types to be filtered in filterAcceptsRow().
+            void setRowFilterTypes(ObserverTreeItem::TreeItemTypeFlags type_flags);
+            //! Gets the tree item types to be filtered in filterAcceptsRow().
+            ObserverTreeItem::TreeItemTypeFlags rowFilterTypes() const;
+
+        protected:
+            bool filterAcceptsRow(int sourceRow, const QModelIndex &sourceParent) const;
+
+        private:
+            ObserverTreeItem::TreeItemTypeFlags row_filter_types;
+        };
+    }
+}
+
+#endif // OBSERVER_TREE_MODEL_PROXY_FILTER_H
