a36b93fb5016768ac08051d4b0737c63cdeaa518
Jaco Naude <naude.jaco@gmail.com>
2010-09-23 04:11:34 +0000

    IFactoryTag namespace update in ObserverHints.cpp

--- src/Core/source/ObserverHints.cpp
+++ src/Core/source/ObserverHints.cpp
@@ -485,7 +485,7 @@ Qtilities::Core::ObserverHints::DragDropFlags Qtilities::Core::ObserverHints::st
     return (DragDropFlags) drag_drop_flags_string.toInt();
 }
 
-Qtilities::Core::Interfaces::IFactoryTag Qtilities::Core::ObserverHints::factoryData() const {
+Qtilities::Core::IFactoryTag Qtilities::Core::ObserverHints::factoryData() const {
     return factoryData();
 }
 
