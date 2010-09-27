3693f4ef7b355236c6fe6a189eb854d4ea1d6ffe
Jaco Naude <naude.jaco@gmail.com>
2010-09-27 20:34:44 +0000

    A new ready to use dynamic side widget is introduced: SideWidgetFileSystem.

---
+++ src/CoreGui/source/SideWidgetFileSystem.cpp
@@ -0,0 +1,112 @@
+/****************************************************************************
+**
+** Copyright (c) 2009-2010, Jaco Naude
+**
+** This file is part of Qtilities which is released under the following
+** licensing options.
+**
+** Option 1: Open Source
+** Under this license Qtilities is free software: you can
+** redistribute it and/or modify it under the terms of the GNU General
+** Public License as published by the Free Software Foundation, either
+** version 3 of the License, or (at your option) any later version.
+**
+** Qtilities is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Qtilities. If not, see http://www.gnu.org/licenses/.
+**
+** Option 2: Commercial
+** Alternatively, this library is also released under a commercial license
+** that allows the development of closed source proprietary applications
+** without restrictions on licensing. For more information on this option,
+** please see the project website's licensing page:
+** http://www.qtilities.org/licensing.html
+**
+** If you are unsure which license is appropriate for your use, please
+** contact support@qtilities.org.
+**
+****************************************************************************/
+
+#include "SideWidgetFileSystem.h"
+#include "ui_SideWidgetFileSystem.h"
+
+#include <QFileSystemModel>
+#include <QTreeView>
+#include <QFileDialog>
+#include <QDir>
+#include <QSettings>
+
+namespace Qtilities {
+    namespace CoreGui {
+        FactoryItem<QWidget, SideWidgetFileSystem> SideWidgetFileSystem::factory;
+    }
+}
+
+struct Qtilities::CoreGui::SideWidgetFileSystemData {
+    SideWidgetFileSystemData(): model(0) {}
+
+    QFileSystemModel *model;
+};
+
+Qtilities::CoreGui::SideWidgetFileSystem::SideWidgetFileSystem(const QString& start_path, QWidget *parent) :
+    QWidget(parent),
+    m_ui(new Ui::SideWidgetFileSystem)
+{
+    m_ui->setupUi(this);
+    d = new SideWidgetFileSystemData;
+
+    d->model = new QFileSystemModel;
+    QDir dir(start_path);
+    if (start_path.isEmpty() || !dir.exists(start_path))
+        d->model->setRootPath(QDir::currentPath());
+    else
+        d->model->setRootPath(start_path);
+    m_ui->treeView->setModel(d->model);
+    m_ui->treeView->hideColumn(1);
+    m_ui->treeView->hideColumn(2);
+    m_ui->treeView->hideColumn(3);
+    m_ui->txtCurrentPath->setText(QDir::currentPath());
+    m_ui->treeView->setRootIndex(d->model->index(QDir::currentPath()));
+    connect(d->model,SIGNAL(rootPathChanged(QString)),SLOT(handleRootPathChanged(QString)));
+    connect(m_ui->btnBrowse,SIGNAL(clicked()),SLOT(handleBtnBrowse()));
+    connect(m_ui->treeView,SIGNAL(doubleClicked(QModelIndex)),SLOT(handleDoubleClicked(QModelIndex)));
+}
+
+void Qtilities::CoreGui::SideWidgetFileSystem::handleRootPathChanged(const QString& newPath) {
+    QDir::setCurrent(newPath);
+}
+
+void Qtilities::CoreGui::SideWidgetFileSystem::handleBtnBrowse() {
+    QString dir = QFileDialog::getExistingDirectory(this, tr("Select Path"),m_ui->txtCurrentPath->text(),QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
+    if (!dir.isNull()) {
+        m_ui->treeView->setRootIndex(d->model->index(dir));
+        m_ui->txtCurrentPath->setText(dir);
+    }
+}
+
+void Qtilities::CoreGui::SideWidgetFileSystem::handleDoubleClicked(const QModelIndex & index) {
+    if (!index.isValid())
+        return;
+
+    if (!d->model)
+        return;
+
+    if (d->model->isDir(index))
+        return;
+
+    QString file_path = d->model->filePath(index);
+    emit requestEditor(file_path);
+}
+
+void Qtilities::CoreGui::SideWidgetFileSystem::setPath(const QString& path) {
+    m_ui->treeView->setRootIndex(d->model->index(path));
+    m_ui->txtCurrentPath->setText(path);
+}
+
+QString Qtilities::CoreGui::SideWidgetFileSystem::path() const {
+    return m_ui->txtCurrentPath->text();
+}
---
+++ src/CoreGui/source/SideWidgetFileSystem.h
@@ -0,0 +1,102 @@
+/****************************************************************************
+**
+** Copyright (c) 2009-2010, Jaco Naude
+**
+** This file is part of Qtilities which is released under the following
+** licensing options.
+**
+** Option 1: Open Source
+** Under this license Qtilities is free software: you can
+** redistribute it and/or modify it under the terms of the GNU General
+** Public License as published by the Free Software Foundation, either
+** version 3 of the License, or (at your option) any later version.
+**
+** Qtilities is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Qtilities. If not, see http://www.gnu.org/licenses/.
+**
+** Option 2: Commercial
+** Alternatively, this library is also released under a commercial license
+** that allows the development of closed source proprietary applications
+** without restrictions on licensing. For more information on this option,
+** please see the project website's licensing page:
+** http://www.qtilities.org/licensing.html
+**
+** If you are unsure which license is appropriate for your use, please
+** contact support@qtilities.org.
+**
+****************************************************************************/
+
+#ifndef SIDE_WIDGET_FILE_SYSTEM_H
+#define SIDE_WIDGET_FILE_SYSTEM_H
+
+#include <Factory>
+#include "QtilitiesCoreGui_global.h"
+
+#include <QObject>
+#include <QtGui>
+
+namespace Ui {
+    class SideWidgetFileSystem;
+}
+
+namespace Qtilities {
+    namespace CoreGui {
+        using namespace Qtilities::Core;
+
+        /*!
+        \struct SideWidgetFileSystemData
+        \brief Structure used by the SideWidgetFileSystem class to store private data.
+          */
+        struct SideWidgetFileSystemData;
+
+        /*!
+        \class SideWidgetFileSystem
+        \brief A widget which provides access to the file system as a side widget.
+
+        <i>This class was added in %Qtilities v0.2.</i>
+          */
+        class QTILITIES_CORE_GUI_SHARED_EXPORT SideWidgetFileSystem : public QWidget
+        {
+            Q_OBJECT
+
+        public:
+            //! Constructs a side widget file system object.
+            /*!
+              \param destination_mode The modes in which this side widget must be available.
+              \param startup_mode The modes in which this side widget must be available when the application starts up.
+              \param start_path The path to use when this widget is constructed. By default it will use QDir::currentPath(), as well as in the case where start_path does not exist.
+              */
+            explicit SideWidgetFileSystem(const QString& start_path = QString(), QWidget *parent = 0);
+            virtual ~SideWidgetFileSystem() {}
+
+            // --------------------------------
+            // Factory Interface Implemenation
+            // --------------------------------
+            static FactoryItem<QWidget, SideWidgetFileSystem> factory;
+
+            //! Function to set the path of this side viewer widget.
+            void setPath(const QString& path);
+            //! Function to get the path of this side viewer widget.
+            QString path() const;
+
+        private slots:
+            void handleRootPathChanged(const QString& newPath);
+            void handleBtnBrowse();
+            void handleDoubleClicked(const QModelIndex& index);
+
+        signals:
+            void requestEditor(const QString& file_name);
+
+        protected:
+            Ui::SideWidgetFileSystem *m_ui;
+            SideWidgetFileSystemData* d;
+        };
+    }
+}
+
+#endif // SCRIPTINGFILESYSTEMWIDGET_H
