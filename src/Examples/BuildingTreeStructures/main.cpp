097cc6b11d0752d47b4b33fe72dec33777d35da8
Jaco Naude <naude.jaco@gmail.com>
2012-01-18 08:04:14 +0200

    Much improved BuildingTreeStructures example.

--- src/Examples/BuildingTreeStructures/main.cpp
+++ src/Examples/BuildingTreeStructures/main.cpp
@@ -32,6 +32,7 @@
 ****************************************************************************/
 
 #include <QApplication>
+#include <QTabWidget>
 
 #include <QtilitiesCoreGui>
 using namespace QtilitiesCoreGui;
@@ -45,115 +46,65 @@ int main(int argc, char *argv[])
     QtilitiesApplication::setApplicationVersion(QtilitiesApplication::qtilitiesVersionString());
 
     // We need to set a QWidget as the application's main window in order for proxy actions to work:
-    QWidget* proxy_widget = new QWidget;
-    QtilitiesApplication::setMainWindow(proxy_widget);
+    QTabWidget* tab_widget = new QTabWidget;
+    QtilitiesApplication::setMainWindow(tab_widget);
 
-    QtilitiesApplication::applicationSessionPath();
     LOG_INITIALIZE();
 
-    // Create the tree nodes:
-    /*TreeNode* nodeA = new TreeNode("Node A");
-    nodeA->enableActivityControl(ObserverHints::CheckboxActivityDisplay,ObserverHints::FollowSelection);
-
-    // Some TreeItem classes:
-    TreeNode* nodeB = nodeA->addNode("Tree Items");
-    nodeB->enableActivityControl(ObserverHints::CheckboxActivityDisplay,ObserverHints::FollowSelection);
-    nodeB->addItem("Item 1");
-    nodeB->addItem("Item 2");
-    nodeB->addItem("Item 3");
-    nodeB->addItem("Item 4");
-    nodeB->addItem("Item 5");
-    nodeB->addItem("Item 6");
-    nodeB->addItem("Item 7");
-
-    // Some TreeFileItem classes:
-    TreeNode* nodeC = nodeA->addNode("File Tree Items");
-    TreeFileItem* file_item = new TreeFileItem("c:/test_dir/test.xml");
-    nodeC->addItem(file_item);*/
-
-    // Add some formatting to the nodes:
-    /*nodeA->setForegroundRole(QBrush(Qt::darkRed));
-    nodeA->setFont(QFont("Helvetica [Cronyx]",20));
-    nodeA->setAlignment(Qt::AlignCenter);
-    nodeA->setBackgroundRole(QBrush(Qt::gray));
-    nodeB->setForegroundRole(QBrush(Qt::darkGreen));
-    nodeB->setFont(QFont("Helvetica [Cronyx]",20));
-    nodeB->setAlignment(Qt::AlignCenter);
-    nodeB->setBackgroundRole(QBrush(Qt::gray));
-    nodeC->setForegroundRole(QBrush(Qt::darkYellow));
-    nodeC->setFont(QFont("Helvetica [Cronyx]",20));
-    nodeC->setAlignment(Qt::AlignCenter);
-    nodeC->setBackgroundRole(QBrush(Qt::gray));*/
-
-    /*nodeC->startProcessingCycle();
-    for (int i = 0; i < 100; i++) {
-        nodeC->addItem(QString("Batch Item").arg(i));
-    }
-    nodeC->endProcessingCycle();*/
-
-    // Test activity:
-    /*QList<QObject*> active_subjects;
-    active_subjects << active_item;
-    nodeB->activityPolicyFilter()->setActiveSubjects(active_subjects);*/
-
-    // Test XML tree streaming:
-    /*QString path_formatted = QString("%1/test_formatted.xml").arg(QtilitiesApplication::applicationSessionPath());
-    nodeA->saveToFile(path_formatted);
-    nodeA->loadFromFile(path_formatted);
-
-    QString path_test = QString("%1/working_tree.xml").arg(QtilitiesApplication::applicationSessionPath());
-    nodeA->addNodeFromFile(path_test);
-
-    // Create an observer widget wih the items:
-    ObserverWidget* tree_widget = new ObserverWidget();
-    QtilitiesApplication::setMainWindow(tree_widget);
-    tree_widget->setObserverContext(nodeA);
-    tree_widget->initialize();
-    tree_widget->show();*/
-
-    // ----------------------------------
+    // ------------------------------------
     // Categorized Tree
     // ------------------------------------
-    TreeNode* rootNode = new TreeNode("Root");
-    rootNode->enableCategorizedDisplay();
-    rootNode->addItem("Child 1",QtilitiesCategory("Category 1"));
-    rootNode->addItem("Child 2",QtilitiesCategory("Category 1"));
-    rootNode->addItem("Child 3");
-    rootNode->addItem("Child 4",QtilitiesCategory("Category 2"));
-    TreeItem* modified_item = rootNode->addItem("Child 5",QtilitiesCategory("Category 2"));
-    rootNode->displayHints()->setModificationStateDisplayHint(ObserverHints::CharacterModificationStateDisplay);
+    TreeNode* rootNodeCategorized = new TreeNode("Root");
+    rootNodeCategorized->enableCategorizedDisplay();
+    // TODO: This breaks the toolbar for some reason... Looks like a display issue since it only happens in QTabWidget:
+    rootNodeCategorized->displayHints()->setDisplayFlagsHint(ObserverHints::ItemView | ObserverHints::ActionToolBar);
+    rootNodeCategorized->displayHints()->setActionHints(ObserverHints::ActionRefreshView);
+    rootNodeCategorized->displayHints()->setCategoryEditingFlags(ObserverHints::CategoriesEditableAllLevels | ObserverHints::CategoriesAcceptSubjectDrops);
+    rootNodeCategorized->displayHints()->setDragDropHint(ObserverHints::AllowDrags);
+    rootNodeCategorized->addItem("Child 1",QtilitiesCategory("Category 1::A",QString("::")));
+    rootNodeCategorized->addItem("Child 2",QtilitiesCategory("Category 1::B",QString("::")));
+    rootNodeCategorized->addItem("Child 3");
+    rootNodeCategorized->addItem("Child 4",QtilitiesCategory("Category 2"));
+    TreeItem* modified_item = rootNodeCategorized->addItem("Child 5",QtilitiesCategory("Category 2"));
+    rootNodeCategorized->displayHints()->setModificationStateDisplayHint(ObserverHints::CharacterModificationStateDisplay);
     modified_item->setModificationState(true);
-//    delete rootNode;
-//    rootNode = 0;
+    ObserverWidget* categorized_widget = new ObserverWidget(rootNodeCategorized);
+    tab_widget->addTab(categorized_widget,QIcon(),"Categorized Tree");
+    categorized_widget->show();
 
     // ----------------------------------
     // Uncategorized Tree
     // ------------------------------------
-    /*TreeNode* rootNode = new TreeNode("Root");
-    TreeNode* parentNode1 = rootNode->addNode("Parent 1");
-    TreeNode* parentNode2 = rootNode->addNode("Parent 2");
+    TreeNode* rootNodeUncategorized = new TreeNode("Root");
+    TreeNode* parentNode1 = rootNodeUncategorized->addNode("Parent 1");
+    TreeNode* parentNode2 = rootNodeUncategorized->addNode("Parent 2");
     parentNode1->addItem("Child 1");
     parentNode1->addItem("Child 2");
     parentNode2->addItem("Child 3");
     parentNode2->addItem("Child 4");
-    parentNode2->addItem("Child 5");*/
+    parentNode2->addItem("Child 5");
 
-    ObserverWidget* uncategorized_widget = new ObserverWidget(rootNode);
+    ObserverWidget* uncategorized_widget = new ObserverWidget(rootNodeUncategorized);
+    tab_widget->addTab(uncategorized_widget,QIcon(),"Uncategorized Tree");
     uncategorized_widget->show();
 
     // ----------------------------------
-    // More Advanced Tree
-    // ------------------------------------
-    /*TreeNode* rootNode = new TreeNode("Root");
-    TreeNode* parentNode1 = rootNode->addNode("Parent 1");
-    parentNode1->enableActivityControl(ObserverHints::CheckboxActivityDisplay,ObserverHints::CheckboxTriggered);
-    TreeNode* parentNode2 = rootNode->addNode("Parent 2");
-    parentNode2->enableNamingControl(ObserverHints::EditableNames,NamingPolicyFilter::ProhibitDuplicateNames);
-    parentNode1->addItem("Child 1");
-    parentNode1->addItem("Child 2");
-    parentNode2->addItem("Child 3");
-    parentNode2->addItem("Child 4");
-    parentNode2->addItem("Child 5");*/
+    // Tree With Subject Filters
+    // ----------------------------------
+    TreeNode* rootNodeWithSubjectFilters = new TreeNode("Root");
+    TreeNode* parentNode1WithSubjectFilters = rootNodeWithSubjectFilters->addNode("Parent 1");
+    parentNode1WithSubjectFilters->enableActivityControl(ObserverHints::CheckboxActivityDisplay,ObserverHints::CheckboxTriggered);
+    TreeNode* parentNode2WithSubjectFilters = rootNodeWithSubjectFilters->addNode("Parent 2");
+    parentNode2WithSubjectFilters->enableNamingControl(ObserverHints::EditableNames,NamingPolicyFilter::ProhibitDuplicateNames);
+    parentNode1WithSubjectFilters->addItem("Child 1");
+    parentNode1WithSubjectFilters->addItem("Child 2");
+    parentNode2WithSubjectFilters->addItem("Try to rename me to Child 3");
+    parentNode2WithSubjectFilters->addItem("Child 3");
+    parentNode2WithSubjectFilters->addItem("Child 4");
+
+    ObserverWidget* subject_filters_widget = new ObserverWidget(rootNodeWithSubjectFilters);
+    tab_widget->addTab(subject_filters_widget,QIcon(),"Tree With Subject Filters");
+    subject_filters_widget->show();
 
     /*rootNode->saveToFile(QtilitiesApplication::applicationSessionPath() + "/example_tree.xml");
     ObserverWidget* uncategorized_widget = new ObserverWidget(rootNode);
@@ -174,5 +125,67 @@ int main(int argc, char *argv[])
     dotGraph.generateDotScript();
     dotGraph.saveToFile(QtilitiesApplication::applicationSessionPath() + "/output_file.gv");*/
 
+    // ----------------------------------
+    // Tree With Formatting
+    // ----------------------------------
+    // Create the tree nodes:
+    TreeNode* nodeAFormatting = new TreeNode("Node A");
+    nodeAFormatting->enableActivityControl(ObserverHints::CheckboxActivityDisplay,ObserverHints::FollowSelection);
+
+    // Some TreeItem classes:
+    TreeNode* nodeBFormatting = nodeAFormatting->addNode("Tree Items");
+    nodeBFormatting->enableActivityControl(ObserverHints::CheckboxActivityDisplay,ObserverHints::FollowSelection);
+    nodeBFormatting->addItem("Item 1");
+    nodeBFormatting->addItem("Item 2");
+    nodeBFormatting->addItem("Item 3");
+    nodeBFormatting->addItem("Item 4");
+    nodeBFormatting->addItem("Item 5");
+    nodeBFormatting->addItem("Item 6");
+    nodeBFormatting->addItem("Item 7");
+
+    // Some TreeFileItem classes:
+    TreeNode* nodeCFormatting = nodeAFormatting->addNode("File Tree Items");
+    TreeFileItem* file_item = new TreeFileItem("c:/test_dir/test.xml");
+    nodeCFormatting->addItem(file_item);
+
+    // Add some formatting to the nodes:
+    nodeAFormatting->setForegroundRole(QBrush(Qt::darkRed));
+    nodeAFormatting->setFont(QFont("Helvetica [Cronyx]",20));
+    nodeAFormatting->setAlignment(Qt::AlignCenter);
+    nodeAFormatting->setBackgroundRole(QBrush(Qt::gray));
+    nodeBFormatting->setForegroundRole(QBrush(Qt::darkGreen));
+    nodeBFormatting->setFont(QFont("Helvetica [Cronyx]",20));
+    nodeBFormatting->setAlignment(Qt::AlignCenter);
+    nodeBFormatting->setBackgroundRole(QBrush(Qt::gray));
+    nodeCFormatting->setForegroundRole(QBrush(Qt::darkYellow));
+    nodeCFormatting->setFont(QFont("Helvetica [Cronyx]",20));
+    nodeCFormatting->setAlignment(Qt::AlignCenter);
+    nodeCFormatting->setBackgroundRole(QBrush(Qt::gray));
+
+    /*nodeCFormatting->startProcessingCycle();
+    for (int i = 0; i < 100; i++) {
+        nodeCFormatting->addItem(QString("Batch Item").arg(i));
+    }
+    nodeCFormatting->endProcessingCycle();*/
+
+    // Test activity:
+    /*QList<QObject*> active_subjects;
+    active_subjects << active_item;
+    nodeBFormatting->activityPolicyFilter()Formatting->setActiveSubjects(active_subjects);*/
+
+    // Test XML tree streaming:
+    /*QString path_formatted = QString("%1/test_formatted.xml").arg(QtilitiesApplication::applicationSessionPath());
+    nodeAFormatting->saveToFile(path_formatted);
+    nodeAFormatting->loadFromFile(path_formatted);
+
+    QString path_test = QString("%1/working_tree.xml").arg(QtilitiesApplication::applicationSessionPath());
+    nodeAFormatting->addNodeFromFile(path_test);*/
+
+    ObserverWidget* formatted_widget = new ObserverWidget(nodeAFormatting);
+    tab_widget->addTab(formatted_widget,QIcon(),"Tree With Formatting");
+    formatted_widget->show();
+
+    tab_widget->resize(600,500);
+    tab_widget->show();
     return a.exec();
 }
