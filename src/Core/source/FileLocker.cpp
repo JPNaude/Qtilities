153fc587eac3f2beb2147b5d5e42eafd437040b8
Jaco Naude <naude.jaco@gmail.com>
2012-09-26 07:53:36 +0200

    Added new FileLocker class.

---
+++ src/Core/source/FileLocker.cpp
@@ -0,0 +1,200 @@
+/****************************************************************************
+**
+** Copyright (c) 2009-2012, Jaco Naude
+**
+** This file is part of Qtilities which is released under the following
+** licensing options.
+**
+** Option 1: Open Source
+** Under this license Qtilities is free software: you can
+** redistribute it and/or modify it under the terms of the GNU General
+** Public License as published by the Free Software Foundation, either
+** version 3 of the License, or (at your option) any later version.
+**
+** Qtilities is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Qtilities. If not, see http://www.gnu.org/licenses/.
+**
+** Option 2: Commercial
+** Alternatively, this library is also released under a commercial license
+** that allows the development of closed source proprietary applications
+** without restrictions on licensing. For more information on this option,
+** please see the project website's licensing page:
+** http://www.qtilities.org/licensing.html
+**
+** If you are unsure which license is appropriate for your use, please
+** contact support@qtilities.org.
+**
+****************************************************************************/
+
+#include "FileLocker.h"
+#include "QtilitiesCoreConstants.h"
+
+#include <QCoreApplication>
+
+#define EXTENSION_LOCK "lck"
+
+using namespace Qtilities::Core;
+
+bool FileLocker::isFileLocked(const QString &file_path) const {
+    QFileInfo fi(file_path);
+    if (!fi.exists())
+        return false;
+
+    // Check if a lock file exists:
+    QFileInfo lock_fi(lockFilePathForFile(file_path));
+    return lock_fi.exists();
+}
+
+bool FileLocker::lockFile(const QString &file_path, QString *errorMsg) {
+    if (isFileLocked(file_path)) {
+        if (errorMsg)
+            *errorMsg = QString(QObject::tr("Cannot lock file that is already locked: ") + file_path);
+        return false;
+    }
+
+    QString lock_file_path = lockFilePathForFile(file_path);
+    QFile lock_file(lock_file_path);
+    if(!lock_file.open(QFile::WriteOnly | QIODevice::Text)) {
+        if (errorMsg)
+            *errorMsg = QString(QObject::tr("Cannot open lock file for writing at: ") + file_path);
+        return false;
+    }
+
+    QString lock_file_string;
+    lock_file_string.append(QObject::tr("Lock file created by ") + QCoreApplication::applicationName() + "\n");
+
+    QString host_name = QObject::tr("Unknown Host");
+    // Get the host name:
+    #ifdef Q_OS_LINUX
+    char* host_name_char = getenv("HOST");
+    #endif
+    #ifdef Q_OS_WIN
+    char* host_name_char = getenv("COMPUTERNAME");
+    #endif
+    if (host_name_char)
+        host_name = host_name_char;
+
+    lock_file_string.append(host_name + "\n");
+    lock_file_string.append(QDateTime::currentDateTime().toString() + "\n");
+
+    if (!lock_file.write(lock_file_string.toAscii())) {
+        if (errorMsg)
+            *errorMsg = QString(QObject::tr("Cannot write lock contents to lock file: ") + file_path);
+        return false;
+    }
+    lock_file.close();
+
+    return true;
+}
+
+bool FileLocker::unlockFile(const QString &file_path, QString *errorMsg) {
+    if (!isFileLocked(file_path)) {
+        if (errorMsg)
+            *errorMsg = QString("Cannot unlock file that is not locked: " + file_path);
+        return false;
+    }
+
+    // Check if a lock file exists:
+    QString lock_file_pat = lockFilePathForFile(file_path);
+    QFile lock_file(lock_file_pat);
+    if (lock_file.remove())
+        return true;
+    else {
+        if (errorMsg)
+            *errorMsg = QString("Cannot unlock file. Failed to delete lock file at: " + lock_file_pat);
+        return false;
+    }
+}
+
+QString FileLocker::lastLockHostName(QString file_path, QString *errorMsg) const {
+    if (!isFileLocked(file_path)) {
+        if (errorMsg)
+            *errorMsg = QString("Cannot unlock file that is not locked: " + file_path);
+        return false;
+    }
+
+    file_path = lockFilePathForFile(file_path);
+
+    QFile lock_file(file_path);
+    if (!lock_file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        if (errorMsg)
+            *errorMsg = QString(QObject::tr("Cannot open lock file for reading at: ") + file_path);
+        return QString();
+    }
+
+    int line_count = 0;
+    QString host_name;
+    while (!lock_file.atEnd()) {
+        QString line = lock_file.readLine();
+        if (line_count == 1) {
+            host_name = line;
+            break;
+        }
+        ++line_count;
+    }
+
+    return host_name;
+}
+
+QString FileLocker::lastLockDateTime(QString file_path, QString *errorMsg) const {
+    if (!isFileLocked(file_path)) {
+        if (errorMsg)
+            *errorMsg = QString("Cannot unlock file that is not locked: " + file_path);
+        return QString();
+    }
+
+    file_path = lockFilePathForFile(file_path);
+
+    QFile lock_file(file_path);
+    if (!lock_file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        if (errorMsg)
+            *errorMsg = QString(QObject::tr("Cannot open lock file for reading at: ") + file_path);
+        return QString();
+    }
+
+    int line_count = 0;
+    QString date_time;
+    while (!lock_file.atEnd()) {
+        QString line = lock_file.readLine();
+        if (line_count == 2) {
+            date_time = line;
+            break;
+        }
+        ++line_count;
+    }
+
+    return date_time;
+}
+
+QString FileLocker::lastLockSummary(QString file_path, const QString &line_break_char, QString *errorMsg) const {
+    if (!isFileLocked(file_path)) {
+        if (errorMsg)
+            *errorMsg = QString("Cannot unlock file that is not locked: " + file_path);
+        return QString();
+    }
+
+    file_path = lockFilePathForFile(file_path);
+
+    QString summary;
+    summary.append(QObject::tr("Locked on host: ") + lastLockHostName(file_path) + line_break_char);
+    summary.append(QObject::tr("Locked at: ") + lastLockDateTime(file_path));
+    return summary;
+}
+
+QString FileLocker::lockFilePathForFile(const QString &file_path) const {
+    QFileInfo fi(file_path);
+    if (!fi.exists())
+        return QString();
+
+    QString lock_file_path = fi.path();
+    lock_file_path.append(QDir::separator());
+    lock_file_path.append(fi.baseName());
+    lock_file_path.append("." + QString(EXTENSION_LOCK));
+
+    return lock_file_path;
+}
---
+++ src/Core/source/FileLocker.h
@@ -0,0 +1,120 @@
+/****************************************************************************
+**
+** Copyright (c) 2009-2012, Jaco Naude
+**
+** This file is part of Qtilities which is released under the following
+** licensing options.
+**
+** Option 1: Open Source
+** Under this license Qtilities is free software: you can
+** redistribute it and/or modify it under the terms of the GNU General
+** Public License as published by the Free Software Foundation, either
+** version 3 of the License, or (at your option) any later version.
+**
+** Qtilities is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Qtilities. If not, see http://www.gnu.org/licenses/.
+**
+** Option 2: Commercial
+** Alternatively, this library is also released under a commercial license
+** that allows the development of closed source proprietary applications
+** without restrictions on licensing. For more information on this option,
+** please see the project website's licensing page:
+** http://www.qtilities.org/licensing.html
+**
+** If you are unsure which license is appropriate for your use, please
+** contact support@qtilities.org.
+**
+****************************************************************************/
+
+#ifndef FILE_LOCKER_H
+#define FILE_LOCKER_H
+
+#include "QtilitiesCore_global.h"
+
+#include <QDateTime>
+
+
+namespace Qtilities {
+    namespace Core {
+        /*!
+        \class FileLocker
+        \brief The FileLocker class provides a simple mechanism to create and manage file locking using a .lck file with lock information.
+
+        The .lck file created when locking a file will contain the date and time that the lock was created,
+        as well as the hostname of the computer used to lock the file.
+
+        <i>This class was added in %Qtilities v1.2.</i>
+          */
+        class QTILIITES_CORE_SHARED_EXPORT FileLocker {
+
+        public:
+            FileLocker() {}
+            virtual ~FileLocker() {}
+
+            // -----------------------------------
+            // Control Over Lock
+            // -----------------------------------
+            //! Checks if the given file is locked.
+            virtual bool isFileLocked(const QString& file_path) const;
+            //! Locks a file.
+            /*!
+             * This function will create a .lck file with the same name as the file name provided. If
+             * the file is already locked, or the .lck file can't be created for some reason, it will return false
+             * and set the errorMsg parameter to the reason why the lock failed.
+             *
+             * \returns True if successfull, false otherwise.
+             */
+            virtual bool lockFile(const QString& file_path, QString* errorMsg = 0);
+            //! Unlocks a file.
+            /*!
+             * This function will create a .lck file with the same name as the file name provided. If
+             * the file is already locked, or the .lck file can't be created for some reason, it will return false
+             * and set the errorMsg parameter to the reason why the lock failed.
+             *
+             * \returns True if successfull, false otherwise.
+             */
+            virtual bool unlockFile(const QString& file_path, QString* errorMsg = 0);
+
+            // -----------------------------------
+            // Details About A Lock
+            // -----------------------------------
+            //! Gets the host name that was used to lock a file.
+            /*!
+             * \brief lastLockHostName
+             * \param file_path The path of the file that is locked, not the lock file itself.
+             * \param errorMsg When valid, will be populated with error message if the function fails.
+             * \return The last lock host name.
+             */
+            virtual QString lastLockHostName(QString file_path, QString *errorMsg = 0) const;
+            //! Gets the date and time when a lock was created.
+            /*!
+             * \brief lastLockHostName
+             * \param file_path The path of the file that is locked, not the lock file itself.
+             * \param errorMsg When valid, will be populated with error message if the function fails.
+             * \return The last lock date and time.
+             */
+            virtual QString lastLockDateTime(QString file_path, QString *errorMsg = 0) const;
+            //! Provides a lock summary string for a lock on a file.
+            /*!
+             * \brief lastLockHostName
+             * \param file_path The path of the file that is locked, not the lock file itself.
+             * \param line_break_char The line break characters to use in the summary text.
+             * \param errorMsg When valid, will be populated with error message if the function fails.
+             * \return The last lock summary.
+             */
+            virtual QString lastLockSummary(QString file_path, const QString& line_break_char = "\n", QString *errorMsg = 0) const;
+
+        private:
+            //! Returns the expected lock file path for a given file path.
+            QString lockFilePathForFile(const QString& file_path) const;
+        };
+    }
+}
+
+
+#endif // FILE_LOCKER_H
